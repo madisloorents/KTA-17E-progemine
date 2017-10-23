7ebf51bc8b71fb5f6530629fec0dcb3db72a1f96
madisloorents <32844602+madisloorents@users.noreply.github.com>
2017-10-23 21:26:19 +0300

    Add files via upload

---
+++ proov/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Jagamisel kuva vastuses (mitu) komakohta? ");
+            string komakoht = Console.ReadLine();
+
+            double aste = Math.Pow(10, Convert.ToDouble(komakoht));
+            Convert.ToDecimal(aste);
+            Console.WriteLine(aste);
+            Console.ReadLine();
+        }
+    }
+}
