9757633715c2b0d4195d4c2f53f4592e1925fba1
madisloorents <32844602+madisloorents@users.noreply.github.com>
2017-10-23 21:48:24 +0300

    Add files via upload

---
+++ If_kask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_kask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            if (number == 5){
+                Console.WriteLine("Number on 5");
+            }
+
+            if (number == 3) {
+                Console.WriteLine("Number on 3");
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
