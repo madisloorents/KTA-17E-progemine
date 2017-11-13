d468d2d1132e393211224a61524051ffd3d67db9
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 14:22:44 +0200

    Create 6by6 korrutustabel

---
+++ Exercises/6by6_korrutamine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6by6_korrutamine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 6;i++)
+            {
+                Console.Write(i + "\t");
+
+                for (int j = 0; j <= 6; j++)
+                {
+                    if (i > 0)
+                    {
+                        Console.Write(i * j + "\t");                   
+                    }
+                    else
+                    {
+                        Console.Write(j + "\t");
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
