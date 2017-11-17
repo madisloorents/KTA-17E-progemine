6320fc13fc1bc251964af2ecebc6c08c11997609
madisloorents <32844602+madisloorents@users.noreply.github.com>
2017-11-17 13:51:20 +0200

    kausta jura

---
+++ Exercises 1/Kalkar2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm.");
+            Console.WriteLine("Ütle mulle 2 numbrit ja ma ütlen Sulle nende korrutise ja jagatise.");
+
+            Console.Write("Esimene number: ");
+            string num1 = Console.ReadLine();
+
+            Console.Write("Teine number: ");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Decimal korrutis = Convert.ToDecimal(num1) * Convert.ToDecimal(num2);
+
+            Console.WriteLine("Korrutades " + num1 + " ja " + num2 + " on tulemuseks: " +(korrutis));
+
+            Decimal jagatis = Convert.ToDecimal(num1) / Convert.ToDecimal(num2);
+
+            jagatis = Math.Truncate(jagatis * 100) / 100;
+
+            Console.WriteLine("Jagades " + num1 + " ja " + num2 + " on tulemuseks: " + (jagatis));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises 1/random number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            int number1 = number.Next(1, 3);
+            Console.WriteLine(number1);
+            Console.ReadLine();
+        }
+    }
+}
